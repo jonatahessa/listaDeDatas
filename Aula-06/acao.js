fd60a28d25ff1609aa636a075f5c53cd21d55640
jonatahessa <jonata.hessa@gmail.com>
2017-03-29 22:35:30 -0300

    oooooooorrra

---
+++ Aula-06/acao.js
@@ -0,0 +1,60 @@
+/* COMMANDOS DE BUSCA NO DOM*/
+  // document.querySelector("SELETOR CSS");
+  // document.querySelectorAll("SELETOR CSS");
+
+  // document.children[0].children[1].children[1];
+  // document.getElementsByTagName("COMANDO HTML");
+  // document.getElementsByClassName("CLASSE");
+  // document.getElementById("ID");
+  // document.getElementsByName("NOME DO CAMPO");
+  // $x("X-PATH");
+
+
+/* COMANDOS COM ELEMENTOS */
+  // elemento.parentNode ==> Acessa o pai do elemento
+  // document.createElement("COMANDO HTML");
+  // pai.appendChild(filho);
+  // elemento.remove(); //CUIDADO NO IE;
+  // elemento.parentNode.removeChild(elmento);
+
+/* COMANDO DE MODIFICAÇÃO */
+  // elemtno.setAttribute("ATTR","VALOR");
+  // elemento.PROPRIEDADE = VALOR;
+  // elemento.style["ESTILO CSS"] = VALOR;
+
+function main(){
+  var btn = document.querySelector("button#btn");
+  btn.addEventListener("click",adicionar);
+}
+
+function adicionar(){
+  var lista = document.querySelector("ol#lista");
+  var conteudo = document.querySelector("input[name=texto]");
+  var itemlista = document.createElement("li");
+  var valor = conteudo.value;
+  lista.appendChild(itemlista);
+
+  if (validar(valor) == true){
+    itemlista.innerHTML = valor + "<button/>";
+  }
+}
+
+function validar(texto){
+  var valido = true;
+  //[TODO] => Validar se o texto é uma data;
+  // EXPRESSAO REGULAR texto.match(/\d\d\d\d-\d\d-\d\d/)
+  // INSTANCIAR DATA Date.parse(texto);
+  // texto[0]
+  // texto.split("CARACTER DE SEPARACAO")
+  return valido;
+}
+
+window.addEventListener("load",main);
+/*
+function ativar(){
+  var paragrafo = document.querySelector("p.ativo");
+  var campo = document.querySelector("input[name=texto]");
+  var texto = campo.value;
+  paragrafo.innerHTML += "<br/>"+texto;
+}
+*/
